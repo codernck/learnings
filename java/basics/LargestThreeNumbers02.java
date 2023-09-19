a102c646c6fcd61249f6081d6b01d1fa4ee926c5
codernck <codernck@gmail.com>
2023-09-19 21:46:42 +0530

    Largest Of Numbers code added

---
+++ java/basics/LargestThreeNumbers02.java
@@ -0,0 +1,84 @@
+package basics;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+public class LargestThreeNumbers02 {
+
+	public static void main(String[] args) {
+		System.out.println("Enter 3 numbers: ");
+		Scanner scn = new Scanner(System.in);
+		int num1 = scn.nextInt();
+		int num2 = scn.nextInt();
+		int num3 = scn.nextInt();
+		
+		System.out.println(findLargest01(num1, num2, num3));
+		System.out.println(findLargest02(num1, num2, num3));
+		System.out.println(findLargest03(num1, num2, num3));
+		System.out.println(findLargest04(num1, num2, num3));
+		System.out.println(findLargest05(num1, num2, num3));
+		System.out.println(findLargest06(num1, num2, num3));
+	}
+
+	private static int findLargest01(int num1, int num2, int num3) {
+		int largest = Integer.MIN_VALUE;
+		
+		if (num1 >= num2 && num1 >= num3) {
+			largest = num1;
+		} else if (num2 >= num1 && num2 >= num3 ) {
+			largest = num2;
+		} else {
+			largest = num3;
+		}
+		return largest;
+	}
+	
+	private static int findLargest02(int num1, int num2, int num3) {
+		int largest = Integer.MIN_VALUE;
+		
+		if (num1 >= num2) {
+			if (num1 >= num3) largest = num1;
+			else largest = num3;
+		} else {
+			if (num2 >= num3 ) largest = num2;
+			else largest = num3;
+		} 
+		
+		return largest;
+	}
+	
+	private static int findLargest03(int num1, int num2, int num3) {
+		int largest = Integer.MIN_VALUE;
+		
+		if (num1 >= num2 && num1 >= num3) {
+			largest = num1;
+		} else if(num2 >= num3) {
+			largest = num2;
+		} else {
+			largest = num3;
+		}
+		
+		return largest;
+	}
+	
+	private static int findLargest04(int num1, int num2, int num3) {
+		int largest = num1 > (num2 > num3 ? num2 : num3) ? num1 : (num2 > num3) ? num2 : num3 ;
+		return largest;
+	}
+	
+	private static int findLargest05(int num1, int num2, int num3) {
+		int largest = Math.max(num1, Math.max(num2, num3));
+		return largest;
+	}
+	
+	private static int findLargest06(int num1, int num2, int num3) {
+		List<Integer> al = new ArrayList<>();
+		al.add(num1);
+		al.add(num2);
+		al.add(num3);
+		
+		return Collections.max(al);
+	}
+}
---
+++ java/basics/LargestTwoNumbers01.java
@@ -0,0 +1,49 @@
+package basics;
+
+import java.util.Scanner;
+
+public class LargestTwoNumbers01 {
+
+	public static void main(String[] args) {
+		System.out.println("Enter two numbers:");
+		Scanner scn = new Scanner(System.in);
+		int num1 = scn.nextInt();
+		int num2 = scn.nextInt();
+		
+		System.out.println(findLargest01(num1, num2));
+		System.out.println(findLargest02(num1, num2));
+		System.out.println(findLargest03(num1, num2));
+	}
+
+	private static int findLargest01(int num1, int num2) {
+		// this if is in separate line to cover this we can write >= instead
+		if (num1 == num2) { 
+			return num1;
+		} else if (num1 > num2) {
+			return num1;
+		} else {
+			return num2;
+		}
+	}
+	
+	private static int findLargest02(int num1, int num2) {
+		// this if is in separate line to cover this we can write >= instead
+		if (num1 == num2) { 
+			return num1;
+		}
+		
+		int res = num1 > num2?num1:num2;
+		return res;
+	}
+	
+	private static int findLargest03(int num1, int num2) {
+		// this if is in separate line to cover this we can write >= instead
+		if (num1 == num2) { 
+			return num1;
+		}
+		
+		int res = Math.max(num1, num2);
+		return res;
+	}
+
+}
