0fd363169f1755c4bad7d1c760e9f0a4b6b79645
codernck <codernck@gmail.com>
2023-09-19 23:45:09 +0530

    Python codes added

---
+++ python/LargestThreeNumbers02.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 19 22:59:16 2023
+
+@author: kiran
+"""
+
+print('Enter the two numbers')
+num1 = int(input())
+num2 = int(input())
+num3 = int(input())
+
+def findLargest01(num1, num2, num3):
+    if num1 >= num2 and num1 >= num3:
+        return num1
+    elif num2 >= num1 and num2 >= num3 :
+        return num2
+    else:
+        return num3
+    
+def findLargest02(num1, num2, num3):
+    if num1 >= num2:
+        if num1 >= num3:
+            return num1
+        else:
+            return num3
+    else:
+        if num2 >= num3:
+            return num2
+        else:
+            return num3
+        
+def findLargest03(num1, num2, num3):
+    if num1 >= num2 and num1 >= num3:
+        return num1
+    elif num2 >= num3:
+        return num2
+    else:
+        return num3
+    
+def findLargest04(num1, num2, num3):
+    largest = num1 if num1 > num2 else num2
+    largest = num3 if num3 > largest else largest
+    return largest
+    
+def findLargest05(num1, num2, num3):
+    return max(num1, max(num2, num3))
+
+def findLargest06(num1, num2, num3):
+    list = [num1, num2, num3]
+    return max(list)
+
+def findLargest07(num1, num2, num3):
+    list = [num1, num2, num3]
+    list.sort()
+    return list[-1]
+        
+
+print(findLargest01(num1, num2, num3)) 
+print(findLargest02(num1, num2, num3))
+print(findLargest03(num1, num2, num3))
+print(findLargest04(num1, num2, num3))
+print(findLargest05(num1, num2, num3))
+print(findLargest06(num1, num2, num3)) 
---
+++ python/LargestTwoNumbers01.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 19 22:39:00 2023
+
+@author: kiran
+"""
+
+print('Enter the two numbers')
+num1 = int(input())
+num2 = int(input())
+
+
+def findLargest01(num1, num2):
+    largest = -1
+    
+    if num1 >= num2:
+        largest = num1
+    else:
+        largest = num2
+        
+    return largest   
+
+def findLargest02(num1, num2):
+    return num1 if num1 >= num2 else num2
+
+def findLargest03(num1, num2):
+    return max(num1, num2)
+    
+def findLargest04(num1, num2):
+    largestNum = lambda a, b:a if a >= b else b
+    return largestNum(num1, num2)
+
+# list comprehension
+def findLargest05(num1, num2):
+    x = [num1 if num1 >= num2 else num2]
+    return x[0]
+
+def findLargest06(num1, num2):
+    x = [num1, num2]
+    x.sort()
+    return x[-1]
+    
+        
+
+print(findLargest01(num1, num2))
+print(findLargest02(num1, num2))
+print(findLargest03(num1, num2))
+print(findLargest04(num1, num2))
+print(findLargest05(num1, num2))
+print(findLargest06(num1, num2))
+        
+    
